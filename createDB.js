e1e6f2437a33cb34fde1eda209ee01f7a2ed43aa
shawnmarkus <shawnmarkus4200@gmail.com>
2022-12-01 17:05:49 +0530

    way to give collection name in mongoose and sorted the fault in mongodb print

--- createDB.js
+++ createDB.js
@@ -1,122 +1,94 @@
-// const MongoClient = require("mongodb").MongoClient;
-// const url = "mongodb://127.0.0.1:27017/employee";
-// // using mongoDB
-// // 1st way
-// const client = new MongoClient(url);
-// (async function () {
-//   try {
-//     // console.log("trying to connect");
-//     // 1st way ----------------------------------------------------------------------------
-//     // await client
-//     //   .connect()
-//     //   .then((db) => {
-//     //     console.log("connected successfully");
-//     //     db.db("RandomData").createCollection("dummyData");
-//     //   })
-//     //   .catch((err) => console.log(err));
-//     // 2nd way ----------------------------------------------------------------------------
-//     await client.connect().then((db) => console.log("connected to db"));
-//     const dummyData = await client.db("RandomData").collection("dummyData");
-//     try {
-//       dummyData
-//         .insertMany([
-//           { _id: 1, category: "cake", type: "chocolate", qty: 10 },
-//           { _id: 2, category: "cake", type: "ice cream", qty: 25 },
-//           { _id: 3, category: "pie", type: "boston cream", qty: 20 },
-//           { _id: 4, category: "pie", type: "blueberry", qty: 15 },
-//           { _id: 5, category: "butterscotch", type: "cake", qty: 10 },
-//           { _id: 6, category: "venilla", type: "ice cream", qty: 25 },
-//           { _id: 7, category: "chocolate", type: "cake", qty: 20 },
-//           { _id: 8, category: "pineapple", type: "cake", qty: 15 },
-//         ])
-//         .then((res) =>
-//           console.log("Number of document inserted : ", res.insertedCount)
-//         )
-//         .catch((err) => {
-//           // console.log(err);
-//           console.log("failed to insert");
-//         });
-//     } catch (err) {
-//       console.log("failed to insert");
-//     }
+const MongoClient = require("mongodb").MongoClient;
+const url2 = "mongodb://127.0.0.1:27017/";
+// using mongoDB
+// 1st way
+const client = new MongoClient(url2);
+(async function () {
+  try {
+    // console.log("trying to connect");
+    // 1st way ----------------------------------------------------------------------------
+    // await client
+    //   .connect()
+    //   .then((db) => {
+    //     console.log("connected successfully");
+    //     db.db("RandomData").createCollection("dummyData");
+    //   })
+    //   .catch((err) => console.log(err));
+    // 2nd way ----------------------------------------------------------------------------
+    await client.connect().then((db) => console.log("connected to db"));
+    const dummyData = await client.db("RandomData").collection("dummyData");
 
-//     // console.log("executing the lower line");
-//     const cursor = dummyData.find({});
-//     await cursor.forEach((doc) => console.log(doc));
+    const insertedData = await dummyData.insertMany([
+      { id: 1, category: "cake", type: "chocolate", qty: 10 },
+      { id: 2, category: "cake", type: "ice cream", qty: 25 },
+      { id: 3, category: "pie", type: "boston cream", qty: 20 },
+      { id: 4, category: "pie", type: "blueberry", qty: 15 },
+      { id: 5, category: "butterscotch", type: "cake", qty: 10 },
+      { id: 6, category: "venilla", type: "ice cream", qty: 25 },
+      { id: 7, category: "chocolate", type: "cake", qty: 20 },
+      { id: 8, category: "pineapple", type: "cake", qty: 15 },
+    ]);
 
-//     // const cursor = dummyData.find({});
-//     // console.log("async");
-//     // for await (const doc of cursor) {
-//     //   console.log(doc);
-//     // }
-//     // docs.forEach((e) => console.log(e));
-//     // console.log(cursor);
-//     // if (cursor) {
-//     //   console.log("printing the docs");
-//     //   await cursor.forEach((instance) => {
-//     //     console.log("hey there");
-//     //     console.log(instance);
-//     //   });
-//     // }
-//   } catch (err) {
-//     console.log(err);
-//   }
-// })();
+    if (insertedData) {
+      console.log("number of data inserted : ", insertedData.insertedCount);
+      const cursor = dummyData.find({});
+      await cursor.forEach((doc) => console.log(doc));
+    }
 
-// connectToDB();
+    // --------------------------- little diff in printing method --------------------------------------
+    /* note if you use then and catch block then nothing is returned to variable to which it is assigned */
+    // await dummyData
+    //   .insertMany([
+    //     { id: 1, category: "cake", type: "chocolate", qty: 10 },
+    //     { id: 2, category: "cake", type: "ice cream", qty: 25 },
+    //     { id: 3, category: "pie", type: "boston cream", qty: 20 },
+    //     { id: 4, category: "pie", type: "blueberry", qty: 15 },
+    //     { id: 5, category: "butterscotch", type: "cake", qty: 10 },
+    //     { id: 6, category: "venilla", type: "ice cream", qty: 25 },
+    //     { id: 7, category: "chocolate", type: "cake", qty: 20 },
+    //     { id: 8, category: "pineapple", type: "cake", qty: 15 },
+    //   ])
+    //   .then(async (data) => {
+    //     console.log("number of data inserted : ", data.insertedCount);
+    //     const cursor = dummyData.find({});
+    //     await cursor.forEach((doc) => console.log(doc));
+    //   })
+    //   .catch((err) => console.log(err));
 
-// // 2nd way - without creating the new instance of the mongodbclient
-// MongoClient.connect(url, function (err, db) {
-//   if (err) throw err;
-//   console.log("connected to db");
-//   var dbObj = db.db("RandomData");
-//   dbObj.createCollection("dummyData", function (err, res) {
-//     if (err) throw err;
-//     console.log("collection created");
-//   });
-// });
+    // --------------------------------------------------------------------------------------------------------------------
+  } catch (err) {
+    console.log(err);
+  }
+})();
+// -------------------------------------------------------------------------------------------------------------------------
 
-// MongoClient.connect(url, function (err, db) {
-//   if (err) throw err;
-//   let dbObj = db.db("RandomData");
-//   // let objPush = []
-//   dbObj.collection("dummyData").insertMany(
-//     [
-//       { _id: 1, category: "cake", type: "chocolate", qty: 10 },
-//       { _id: 2, category: "cake", type: "ice cream", qty: 25 },
-//       { _id: 3, category: "pie", type: "boston cream", qty: 20 },
-//       { _id: 4, category: "pie", type: "blueberry", qty: 15 },
-//     ],
-//     function (err, res) {
-//       if (err) throw err;
-//       console.log("object inserted");
-//     }
-//   );
-// });
-
-// ------------------------------------------------------------------------------------------------------------------------------
 // using mongoose
-const url = "mongodb://127.0.0.1:27017/RandomData";
 
 const mongoose = require("mongoose");
+const url = "mongodb://127.0.0.1:27017/RandomData";
 mongoose
   .connect(url)
   .then((db) => console.log("connected to db "))
   .catch((err) => console.log("not connected"));
 
-const dummySchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    unique: true,
+const dummySchema = new mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      unique: true,
+    },
+    category: String,
+    type: String,
+    qty: Number,
   },
-  category: String,
-  type: String,
-  qty: Number,
-});
 
-const model = mongoose.model("dummy", dummySchema);
+  { collection: "dummyCheck" } //this is the way to give the specific collection name.
+);
 
-model
+// MODEL : A model is a class with which we construct documents. In this case, each document will be a dummy with properties and behaviors as declared in our schema.
+const insertionModel = mongoose.model("dummy", dummySchema);
+
+const insertedData = insertionModel
   .insertMany([
     { id: 1, category: "cake", type: "chocolate", qty: 10 },
     { id: 2, category: "cake", type: "ice cream", qty: 25 },
@@ -127,7 +99,36 @@ model
     { id: 7, category: "chocolate", type: "cake", qty: 20 },
     { id: 8, category: "pineapple", type: "cake", qty: 15 },
   ])
-  .then((data) => console.log(data));
+  .then((data) =>
+    data.forEach((doc) => {
+      console.log(doc);
+    })
+  )
+  .catch((err) => console.log(err.errmsg));
+
+//just as a use case
+// (async function () {
+//   const dummyDataInsertionCheck = new insertionModel({
+//     id: 9,
+//     category: "cake",
+//     type: "pastry",
+//     qty: 100,
+//   });
+
+//   await dummyDataInsertionCheck
+//     .save()
+//     .then((succ) => {
+//       console.log("inserted one doc");
+//     })
+//     .catch((err) => console.log(err.errmsg));
+// })();
+
+(async function () {
+  const cursor = await insertionModel.find({});
+  // await cursor.array.forEach(element => {
+  await cursor.forEach((doc) => console.log(doc));
+  // });
+})();
 
 //  my question is that we received what , in db
 
