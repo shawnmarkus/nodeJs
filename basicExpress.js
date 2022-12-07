21ff24045839315de9e66d5a4bdecfd7fcb4c99f
shawnmarkus <shawnmarkus4200@gmail.com>
2022-12-07 23:09:51 +0530

    tried middleware

--- basicExpress.js
+++ basicExpress.js
@@ -14,6 +14,23 @@ app.get("/", async function (req, res) {
   }
 });
 
+function middleware(req, res, next) {
+  // if actual address is https://example.com/creatures?filter=sharks
+  console.log(req.protocol); // "https"
+  console.log(req.hostname); // "example.com"
+  console.log(req.path); // "/creatures"
+  // console.log(req.originalUrl); // "/creatures?filter=sharks"
+  // console.log(req.subdomains);
+  next();
+}
+
+app.all("/test", middleware, async function (req, res) {
+  let allData = await insertionModel.find({});
+  if (allData) {
+    res.json({ allData, msg: "you retrived the data successfully " });
+  }
+});
+
 app.post("/users", function (req, res) {
   res.send(`welcome to the users space \n`);
 });
